146605d2c75ddef08248830fde5ce4bbcb6eef3b
Luis Suarez <suarezluis@gmail.com>
2019-04-14 15:52:50 -0500

    Updates

---
+++ precache-manifest.114593a7e06df553e8d868091eefe6f5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "458a39205caec5b5c90b",
+    "url": "/game-of-thrones/static/js/runtime~main.2bb8da27.js"
+  },
+  {
+    "revision": "682de2352aec199ebc5c",
+    "url": "/game-of-thrones/static/js/main.b2803f15.chunk.js"
+  },
+  {
+    "revision": "bf4533870923bde5d6e5",
+    "url": "/game-of-thrones/static/js/2.ba01c51e.chunk.js"
+  },
+  {
+    "revision": "682de2352aec199ebc5c",
+    "url": "/game-of-thrones/static/css/main.c8675b24.chunk.css"
+  },
+  {
+    "revision": "ddebe09fe6b7bf59481e0720ca0df43f",
+    "url": "/game-of-thrones/index.html"
+  }
+];
